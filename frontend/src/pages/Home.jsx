0b2c8a91a6dec8a309d41e6fcdde812d104192cf
Pratham shetty <prathamshetty484@gmail.com>
2024-12-11 22:58:28 +0530

    added register login and home page ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,16 +15,16 @@ export default function Home() {
             <div className="absolute top-0 left-0 w-full h-full bg-[url('./assets/bg.jpg')] bg-cover bg-center bg-fixed opacity-90"></div>
             
             {/* Navbar */}
-            <nav className="w-full bg-black bg-opacity-50 text-white p-4 flex justify-between items-center absolute top-0 left-0 z-10">
-                <div className="text-2xl font-bold ">Roamio</div>
-                <div className="space-x-4">
-                    <button className="text-lg hover:text-blue-400">Home</button>
-                    <button className="text-lg hover:text-blue-400">Dashboard</button>
-                    <button className="text-lg hover:text-blue-400">Profile</button>
+            <nav className="w-full bg-black bg-opacity-60 text-white p-4 flex justify-between items-center absolute top-0 left-0 z-10">
+                <div className="text-2xl font-bold px-6">Roamio</div>
+                <div className="space-x-9">
+                    <button className="text-xl hover:text-blue-400 font-semibold">Home</button>
+                    <button className="text-xl hover:text-blue-400 font-semibold">Dashboard</button>
+                    <button className="text-xl hover:text-blue-400 font-semibold">Profile</button>
                     {/* Logout Button in Navbar */}
                     <button
                         onClick={handleClick1}
-                        className="text-lg hover:text-blue-400"
+                        className="text-xl hover:text-blue-400 font-semibold"
                     >
                         Logout
                     </button>
@@ -35,8 +35,7 @@ export default function Home() {
             <div className="flex flex-col items-center justify-center text-center text-white relative z-10 mt-[100px]">
                 <h1 className="text-7xl font-extrabold mb-4 text-red-900">Roamio</h1>
                 <p className="text-2xl max-w-3xl mb-8 px-4 text-red-950 font-semibold">
-                    Roamio is your ultimate travel companion, offering a seamless and immersive journey into nature's beauty. 
-                    Explore breathtaking destinations and uncover hidden gems around the world with ease. 
+                Roamio is an AI-driven travel planner that simplifies trip organization, offers real-time expense tracking, AI-generated itineraries, and dynamic recommendations. With features like travel badges, clan affiliations, and challenges, it delivers a fun, personalized travel experience.
                 </p>
                 <button
                     onClick={handleClick1}
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,79 +1,91 @@
 import { loginRoute } from "../utils/APIRoutes";
-
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
-import {toast} from "react-hot-toast";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
 import axios from "axios";
 
-export default function Login(){
-    const navigate=useNavigate();
-    
-    
-    const [loginData,setLoginData]=useState({
-        username:"",
-        password:""
+export default function Login() {
+  const navigate = useNavigate();
+
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleUserInput(e) {
+    const { name, value } = e.target;
+    setLoginData({
+      ...loginData,
+      [name]: value,
     });
-    
-    function handleUserInput(e){
-        const {name,value}=e.target;
-        setLoginData({
-            ...loginData,
-            [name]:value
-        })
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    if (!loginData.password || !loginData.username) {
+      toast.error("Please fill all details");
+      return;
     }
-    async function handleSubmit(event){
-        event.preventDefault();
-        if(!loginData.password || !loginData.username){
-            toast.error("Please fill all details");
-            return;
+    const { data } = await axios.post(loginRoute, loginData);
 
-        }
-        const { data } = await axios.post(loginRoute,loginData);
-    
-          if (data.status === false) {
-            toast.error(data.msg);
-          }
-          if (data.status === true) {
-            localStorage.setItem('login-user',JSON.stringify(data.user));
-            toast.success('suggessfull login')
-            navigate("/home");
-            setLoginData({
-                username:"",
-                password:"",
-                
-            });
-          }
-        
-            }
-            useEffect(() => {
-              //localStorage.clear();
-                if (localStorage.getItem('login-user')) {
-                  navigate("/home");
-                }
-              }, []); 
-    return ( <div className="h-[100vh] w-[100vw] bg-[#131324] flex items-center justify-center">
-        <form onSubmit={handleSubmit} className="bg-[#00000076] flex flex-col rounded-3xl px-[4rem] pt-[2.5rem] pb-[2rem] shadow-[0_0_5px_gray] gap-2">
-            <div className="flex">
-                <h1 className="font-bold text-white text-3xl">Roamio</h1>
-            </div>
-            <input type="text" placeholder="Enter UserName" name="username" value={loginData.username} onChange={handleUserInput} className="bg-transparent p-[1rem] border-[0.1rem] border-[#4e0eff] rounded-2xl font-[1rem] text-white w-[100%] my-[1rem]"/>
-            
-            <input type="password" placeholder="Enter password" name="password" value={loginData.password} onChange={handleUserInput} className="bg-transparent p-[1rem] border-[0.1rem] border-[#4e0eff] rounded-2xl font-[1rem] text-white w-[100%] mb-[1rem]"/>
-            
-            <button type="submit" className="bg-[#997af0] hover:bg-[#4e0eff] p-[1rem] border-[0.1rem]  rounded-2xl font-[1rem] text-white w-[100%] mb-[0.5rem] ">Click</button>
-            <span className="text-white mx-[1rem]">Dont have an account??   <Link to="/register" className="text-blue-500">Register</Link></span>
-        
-        
-        </form>
-    
-        
-        
-        
-        
-        
-        
-        
-        </div>)
+    if (data.status === false) {
+      toast.error(data.msg);
+    }
+    if (data.status === true) {
+      localStorage.setItem("login-user", JSON.stringify(data.user));
+      toast.success("Successful login");
+      navigate("/home");
+      setLoginData({
+        username: "",
+        password: "",
+      });
+    }
+  }
 
+  useEffect(() => {
+    if (localStorage.getItem("login-user")) {
+      navigate("/home");
+    }
+  }, []);
+
+  return (
+    <div className="h-[100vh] w-[100vw] bg-[url('./assets/bg.jpg')] bg-cover bg-center bg-fixed flex items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-[#00000045] flex flex-col rounded-3xl px-[4rem] pt-[2.5rem] pb-[2rem] shadow-[0_0_5px_gray] gap-2"
+      >
+        <div className="flex items-center justify-center">
+          <h1 className="font-bold text-4xl text-[#7a1919]">Roamio</h1>
+        </div>
+        <input
+          type="text"
+          placeholder="Enter Username"
+          name="username"
+          value={loginData.username}
+          onChange={handleUserInput}
+          className="bg-transparent p-[1rem] border-[0.1rem] border-[#6f1420] rounded-2xl font-[1rem] text-[#ffffff] w-[100%] my-[1rem]"
+        />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          name="password"
+          value={loginData.password}
+          onChange={handleUserInput}
+          className="bg-transparent p-[1rem] border-[0.1rem] border-[#6f1420] rounded-2xl font-[2rem] text-[#ffffff] w-[100%] mb-[1rem]"
+        />
+        <button
+          type="submit"
+          className="bg-[#651225] hover:bg-[#ebe7ff] text-white hover:text-[#651225] p-[1rem]  rounded-2xl font-[1rem] w-[100%] mb-[0.5rem]"
+        >
+          Login
+        </button>
+        <span className="text-[#ebebeb] mx-[1rem] text-lg">
+          Don't have an account?{" "}
+          <Link to="/register" className="text-[#00bcd4] hover:text-[#4e0eff]">
+            Register
+          </Link>
+        </span>
+      </form>
+    </div>
+  );
 }
